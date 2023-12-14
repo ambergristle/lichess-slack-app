23f8678f6ebef5b6df3ae7988961828aaa853065
ambergristle <101149854+ambergristle@users.noreply.github.com>
2023-12-14 07:29:57 -0800

    oops

---
+++ src/config.ts
@@ -0,0 +1,23 @@
+/** @todo */
+const STATE = 'WHAT_IS_THIS';
+
+const BASE_URL = 'https://firefly-pumped-trivially.ngrok-free.app';
+
+const SLACK_CLIENT_ID = process.env.SLACK_CLIENT_ID;
+const SLACK_CLIENT_SECRET = process.env.SLACK_CLIENT_SECRET;
+
+const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN;
+
+if (!SLACK_CLIENT_ID || !SLACK_CLIENT_SECRET || !SLACK_BOT_TOKEN) {
+  throw new Error('Slack credentials missing');
+}
+
+export default {
+  STATE,
+  BASE_URL,
+  REGISTRATION_URL: `${BASE_URL}/slack/register`,
+  SLACK_CLIENT_ID,
+  SLACK_CLIENT_SECRET,
+  
+  SLACK_BOT_TOKEN,
+};
