274e28a2ba1aa39c6c3565f41d09dcd86788b76c
ambergristle <101149854+ambergristle@users.noreply.github.com>
2024-02-21 19:23:13 -0800

    chore: moved utils to separate files

---
+++ src/lib/utils/urls.ts
@@ -0,0 +1,19 @@
+
+export const constructHref = (
+  baseUrl: string, 
+  params?: Record<string, string>
+) => {
+  const url = new URL(baseUrl);
+    
+  if (!params) return url.href;
+
+  Object
+    .entries(params)
+    .forEach(([key, value]) => {
+      /** @todo error handling */
+      if (typeof value !== 'string') throw `Invalid parameter type ${typeof value}`;
+      url.searchParams.set(key, value);
+    });
+
+  return url.href;
+};
