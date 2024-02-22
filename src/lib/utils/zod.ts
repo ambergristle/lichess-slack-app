d9e2d757827298ff938f60d487b92d41c0468d00
ambergristle <101149854+ambergristle@users.noreply.github.com>
2024-02-21 19:18:15 -0800

    feat: create validation parser

---
+++ src/lib/utils/zod.ts
@@ -0,0 +1,35 @@
+import { ZodSchema } from 'zod';
+import { ValidationError } from '../errors';
+
+type ParserFactoryOptions = {
+  documentName: string;
+  errorMessage: string;
+}
+
+export const parserFactory = <Z extends ZodSchema>(schema: Z, options: ParserFactoryOptions) => {
+  return (data: unknown): Z['_output'] => {
+    const result = schema.safeParse(data)
+    if (result.success) return result.data;
+
+    const errors = result.error.issues.map((issue) => {
+      const path = Array.isArray(issue.path) 
+        ? issue.path.join('.') 
+        : issue.path;
+
+      return {
+        path,
+        message: issue.message,
+      }
+    })
+
+    const {
+      errorMessage,
+      documentName
+    } = options;
+
+    throw new ValidationError(errorMessage, {
+      documentName,
+      errors
+    })
+  }
+}
